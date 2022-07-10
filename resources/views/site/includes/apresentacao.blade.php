6ca056c7d58d88cca877163428b9c25681db6c02
gutemberg <gutembergcosta01@gmail.com>
2022-07-10 20:10:55 -0300

    start

---
+++ resources/views/site/includes/apresentacao.blade.php
@@ -0,0 +1,21 @@
+<section id="apresentacao">
+
+    <div class="pagina">
+        <div class="area-titular" >
+            <div class="titl" >
+                <h2>
+                    Bem Vindos ao Consórcio Net Brasil
+                </h2>
+                
+            </div>
+
+            <div class="texto-central">
+                {!! $dadosGerais['intro_site'] !!}
+                
+            </div>
+        </div>
+    </div>
+    
+    
+
+</section>
---
+++ resources/views/site/inicio.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.site',[$dadosGerais,$produtos,])
+
+@section('pagina')
+
+    @include('site.includes.slider-area', [$slides])
+    @include('site.includes.produtos',[$dadosGerais,$produtos,])
+    @include('site.includes.numeros',[$dadosGerais,$numeros])
+    {{--@include('site.includes.marcas',[$dadosGerais])--}}
+
+@endsection
+

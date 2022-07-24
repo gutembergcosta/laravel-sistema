1b0c20dace1468dd2f00c1bfd403d46698357668
gutemberg <gutembergcosta01@gmail.com>
2022-07-24 20:46:23 -0300

    declarar actions no construtor

--- app/Http/Controllers/TabelaController.php
+++ app/Http/Controllers/TabelaController.php
@@ -3,26 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Tabela;
 use App\Http\Requests\TabelaRequest;
 use App\Actions\Tabela\SalvarTabela;
 use App\Actions\Tabela\DataFormTabela;
-use App\Actions\Arquivo\ExcluirArquivo;
+use App\Actions\Tabela\DestroyTabela;
+use App\Actions\Tabela\ListTabela;
 
 class TabelaController extends Controller
 {
+    private $dataFormTabela;
+    private $salvarTabela;
+    private $destroyTabela;
+    private $listTabela;
 
-    public function __construct(DataFormTabela $dataFormTabela)
+    public function __construct(
+        DataFormTabela $dataFormTabela,
+        SalvarTabela $salvarTabela,
+        DestroyTabela $destroyTabela,
+        ListTabela $listTabela
+    )
     {
         $this->dataFormTabela = $dataFormTabela;   
+        $this->salvarTabela = $salvarTabela;   
+        $this->destroyTabela = $destroyTabela;   
+        $this->listTabela = $listTabela;   
     }
   
     public function lista()
     {
-        $tipoUser = (Auth::user()->userDados->tipo == 'admin') ? 'admin' : 'user';
-        $lista = Tabela::all();
-        return view('painel.tabelas.lista-'.$tipoUser, compact('lista'));
+        $data = $this->listTabela->list();
+        $lista = $data['lista'];
+        return view("painel.tabelas.lista-{$data['tipoUser']}", compact('lista'));
     }
     
     public function formulario($action, $id = null)
@@ -32,19 +43,15 @@ class TabelaController extends Controller
     }
 
     
-    public function salvar(TabelaRequest $request, SalvarTabela $salvarTabela)
+    public function salvar(TabelaRequest $request)
     {
-        $data = $salvarTabela->execute($request);
+        $data = $this->salvarTabela->execute($request);
         return response()->json($data);
-        
     }
     
-    public function destroy(Request $request, ExcluirArquivo $excluirArquivo)
+    public function destroy(Request $request)
     {
-        $item       = Tabela::find($request->id);
-        $excluirArquivo->destroyGaleria($item->ref);
-        //$item->delete();
-        return response()->json(['msg' =>'Item excluido com sucesso']);
-
+        $data = $this->destroyTabela->delete($request->id);
+        return response()->json(['msg' => $data['msg']]);
     }
 }

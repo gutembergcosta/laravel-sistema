9b23475648e92b3d3dd96a9d81053751399887cd
gutemberg <gutembergcosta01@gmail.com>
2022-07-24 18:54:21 -0300

    refactor de tabela controller

--- app/Http/Controllers/TabelaController.php
+++ app/Http/Controllers/TabelaController.php
@@ -5,74 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Tabela;
-use Validator;
-use App\Http\Requests\StoreTabelaRequest;
+use App\Http\Requests\TabelaRequest;
+use App\Actions\Tabela\SalvarTabela;
+use App\Actions\Tabela\DataFormTabela;
+use App\Actions\Arquivo\ExcluirArquivo;
 
 class TabelaController extends Controller
 {
-   
-    public function index()
+
+    public function __construct(DataFormTabela $dataFormTabela)
+    {
+        $this->dataFormTabela = $dataFormTabela;   
+    }
+  
+    public function lista()
     {
         $tipoUser = (Auth::user()->userDados->tipo == 'admin') ? 'admin' : 'user';
         $lista = Tabela::all();
         return view('painel.tabelas.lista-'.$tipoUser, compact('lista'));
     }
-
-    public function show($id)
+    
+    public function formulario($action, $id = null)
     {
-        //
-    }
-
- 
-    public function edit($id)
-    {
-        //
+        $data = $this->dataFormTabela->getData($action, $id);
+        return view('painel.tabelas.formulario', $data);
     }
 
     
-    public function create()
+    public function salvar(TabelaRequest $request, SalvarTabela $salvarTabela)
     {
-        $ref = uniqid();
-        $actionForm = 'admin/salvar-tabela';
-        $item = [];
-        $galeria = FALSE;
-        $img = '';
-        $action = 'add';
-
-        $dataArquivoDestaque = ['ref' => $ref, 'img' => $img, 'tipo' => 'tabela','titulo' => 'Tabela'];
-
-        $compact = compact('ref', 'item','action','dataArquivoDestaque','actionForm');
-     
-        return view('painel.tabelas.formulario', $compact);
+        $data = $salvarTabela->execute($request);
+        return response()->json($data);
+        
     }
-
     
-    public function store(StoreTabelaRequest $request)
+    public function destroy(Request $request, ExcluirArquivo $excluirArquivo)
     {
-        $frmData = [];
+        $item       = Tabela::find($request->id);
+        $excluirArquivo->destroyGaleria($item->ref);
+        //$item->delete();
+        return response()->json(['msg' =>'Item excluido com sucesso']);
 
-        $frmData['msg']         = 'Item salvo com sucesso';
-        $frmData['destino']     = 'reload';
-
-        $request['slug']        = slug($request->nome);
-
-        Tabela::Create($request->all());
- 
-        echo json_encode($frmData);
-    }
-
-
-    
-
-    
-    public function update(Request $request, $id)
-    {
-        dd('update');
-    }
-
-    
-    public function destroy($id)
-    {
-        dd('destroy');
     }
 }
